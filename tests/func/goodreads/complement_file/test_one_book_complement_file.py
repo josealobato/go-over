a2684094d6752aa826cad408aea7de0d9bd3a2a5
Jose A Lobato <21879+josealobato@users.noreply.github.com>
2022-02-26 13:13:21 +0100

    shuffle test

--- tests/func/goodreads/complement_file/test_one_book_complement_file.py
+++ tests/func/goodreads/complement_file/test_one_book_complement_file.py
@@ -1,7 +1,7 @@
 from typing import Dict
 import pytest
 
-from .func_test_tools import load_result
+from ...func_test_tools import load_result
 
 # Under test
 from go_over.goodreads.processor import process
